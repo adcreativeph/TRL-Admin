9cea945e09502615ec8ca150642eed4768bcc063
Alfred D <alfred.dagohoy@gmail.com>
2018-02-22 15:27:16 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ define('APP_FOLDER','/admin');
 
 require_once BASE_PATH.'/lib/MysqliDb.php';
 
-// $servername = "thereallifebo.c8js7xop6tz8.us-east-2.rds.amazonaws.com";
 $servername = "localhost";
 $username = "treall";
 $password = "soojelay6209";
