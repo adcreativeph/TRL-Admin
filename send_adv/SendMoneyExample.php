6e34fde67bab68bd0842735a04df25d1ed4881d3
Normaneil <normaneil.macutay@gmail.com>
2018-02-26 20:06:30 +0800

    working sample script of advcash api

--- send_adv/SendMoneyExample.php
+++ send_adv/SendMoneyExample.php
@@ -6,16 +6,16 @@ require_once("MerchantWebService.php");
 $merchantWebService = new MerchantWebService();
 
 $arg0 = new authDTO();
-$arg0->apiName = "http://18.217.136.176/admin/api/api.php";
+$arg0->apiName = "thereallife";
 $arg0->accountEmail = "www.thereallife@gmail.com";
-$arg0->authenticationToken = $merchantWebService->getAuthenticationToken("098f6bcd4621d373cade4e832627b4f6");
+$arg0->authenticationToken = $merchantWebService->getAuthenticationToken("thereallifeuser");
 
 $arg1 = new sendMoneyRequest();
 $arg1->amount = 1.00;
-$arg1->currency = "USD";
-$arg1->email = "receiver_email";
+$arg1->currency = "EUR";
+$arg1->email = "julzinvest2@gmail.com";
 //$arg1->walletId = "U000000000000";
-$arg1->note = "note";
+$arg1->note = "this is a test";
 $arg1->savePaymentTemplate = false;
 
 $validationSendMoney = new validationSendMoney();
@@ -26,15 +26,14 @@ $sendMoney = new sendMoney();
 $sendMoney->arg0 = $arg0;
 $sendMoney->arg1 = $arg1;
 
-print_r($validationSendMoney);
-// try {
-//     $merchantWebService->validationSendMoney($validationSendMoney);
-//     $sendMoneyResponse = $merchantWebService->sendMoney($sendMoney);
+try {
+    $merchantWebService->validationSendMoney($validationSendMoney);
+    $sendMoneyResponse = $merchantWebService->sendMoney($sendMoney);
 
-//     echo print_r($sendMoneyResponse, true)."<br/><br/>";
-//     echo $sendMoneyResponse->return."<br/><br/>";
-// } catch (Exception $e) {
-//     echo "ERROR MESSAGE => " . $e->getMessage() . "<br/>";
-//     echo $e->getTraceAsString();
-// }
+    echo print_r($sendMoneyResponse, true)."<br/><br/>";
+    echo $sendMoneyResponse->return."<br/><br/>";
+} catch (Exception $e) {
+    echo "ERROR MESSAGE => " . $e->getMessage() . "<br/>";
+    echo $e->getTraceAsString();
+}
 ?>
